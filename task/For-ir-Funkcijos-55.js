b66beefdab5bdcb29d6ef896cc1153600594f449
Faustas <faustas.slekonis@gmail.com>
2025-04-02 15:01:26 +0300

    js scriptai

--- task/For-ir-Funkcijos-55.js
+++ task/For-ir-Funkcijos-55.js
@@ -286,3 +286,74 @@ console.log(`zdije yra e skaicius raidziu ${kiekYraE}`);
 // e raidė yra 4 vietoje 
 // 25. Parašykite for ciklą, kuris išvestų kokia kintamojo let Lobis = 150 reikšmė būtų, jei 
 // mes iš jo atimtume reikšmes nuo 20 iki 15. Į consolę išveskite kaip mažėja turtas.
+
+
+//1. Parašykite funkciją, kuri grąžina dviejų skaičių sumą.
+function suma20(a, b) {
+    return a + b;
+}
+
+console.log(suma20(5, 3));
+console.log('---------------')
+
+// 2. Parašykite funkciją, kuri grąžina dviejų skaičių skirtumą. 
+
+function skirtumas(a, b) {
+    return a - b; 
+}
+
+console.log(skirtumas(8, 3)); 
+console.log('---------------')
+
+// 3. Parašykite funkciją, kuri grąžina dviejų skaičių sandaugą. 
+function sandauga(a, b) {
+    return a * b; 
+}
+
+console.log(sandauga(4, 5)); 
+console.log('---------------')
+
+// 4. Parašykite funkciją, kuri grąžina dviejų skaičių dalybą. 
+function dalyba(a, b) {
+    return a / b; 
+}
+
+console.log(dalyba(4, 5)); 
+console.log('---------------')
+
+// 5. Parašykite funkciją, kuri grąžina dviejų skaičių vidurkį. 
+// 6. Parašykite funkciją, kuri patikrina, ar skaičius yra teigiamas. 
+// 7. Parašykite funkciją, kuri patikrina, ar skaičius yra lyginis. 
+// 8. Parašykite funkciją, kuri patikrina, ar skaičius yra nelyginis. 
+// 9. Parašykite funkciją, kuri patikrina, ar skaičius dalijasi iš 5 be liekanos. 
+// 10. Parašykite funkciją, kuri patikrina, ar skaičius yra lygus 100. 
+// 11. Parašykite funkciją, kuri patikrina, ar skaičius yra didesnis nei 10. 
+// 12. Parašykite funkciją, kuri grąžina didesnį iš dviejų skaičių. 
+// 13. Parašykite funkciją, kuri grąžina mažesnį iš dviejų skaičių. 
+// 14. Parašykite funkciją, kuri patikrina, ar trys skaičiai yra vienodi. 
+// 15. Parašykite funkciją, kuri patikrina, ar du skaičiai yra lygūs. 
+// 16. Parašykite funkciją, kuri patikrina, ar skaičius yra tarp 1 ir 10. 
+// 17. Parašykite funkciją, kuri patikrina, ar skaičius yra tarp 50 ir 100. 
+// 18. Parašykite funkciją, kuri patikrina, ar skaičius yra neigiamas. 
+// 19. Parašykite funkciją, kuri grąžina „Teigiamas“ arba „Neigiamas“ priklausomai nuo 
+// skaičiaus, kurį nusiunčiate funkcijai. 
+// 20. Parašykite funkciją, kuri patikrina, ar skaičius yra didesnis nei 18 (pvz., patikrinimas, 
+// ar žmogus pilnametis). 
+// 21. Parašykite funkciją, kuri patikrina, ar perduotas žodis yra „labas“. 
+// 22. Parašykite funkciją, kuri patikrina, ar žodis yra ilgesnis nei 5 simboliai. 
+// 23. Parašykite funkciją, kuri grąžina pirmąją perduoto žodžio raidę. 
+// 24. Parašykite funkciją, kuri grąžina paskutinę perduoto žodžio raidę. 
+// 25. Parašykite funkciją, kuri patikrina, ar žodis prasideda raide „A“. 
+// 26. Parašykite funkciją, kuri grąžina žodžio ilgį. 
+// 27. Parašykite funkciją, kuri grąžina „Ilgas“ arba „Trumpas“, priklausomai nuo žodžio 
+// ilgio. Ilgas žodis yra 7 simbolių ilgio arba didesnis, o trumpas yra 6 simbolių ilgio arba 
+// trumpesnis 
+// 28. Parašykite funkciją, kuri patikrina, ar du žodžiai yra vienodi. 
+// 29. Parašykite funkciją, kuri grąžina „Diena“ arba „Naktis“, priklausomai nuo perduotos 
+// valandos (0–23). 
+// 30. Parašykite vieną funkciją, kuri patikrina skaičiaus vertę, ir jęi: 
+// a. Skaičius yra teigiamas, išveda į console ‘esu teigiamas’ 
+// b. Skaičius yra neigiamas, išveda į console ‘esu neigiamas’ 
+// c. Skaičius yra nulis, išveda į console ‘esu nulis’ 
+// d. Skaičius yra didesnis už 100, išveda ‘didžiulis skaičius’ 
+// e. Jei skaičius nėra didesnis už 100, išveda ‘esu mažiukas
