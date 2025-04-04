a46c38bedd8cd79231d13a047af7b873ccd76109
Faustas <faustas.slekonis@gmail.com>
2025-04-04 13:57:10 +0300

    js scriptai

--- codewar/codewar1.js
+++ codewar/codewar1.js
@@ -86,3 +86,28 @@ function reverseWords(str){
 
   console.log(solution('45', '1'), '1451');
   console.log(solution('13', '200'), '1320013');
+
+
+
+
+  function whatday(num) { 
+    if(num === 1){
+      return 'Sunday'
+    } if(num === 2){
+      return 'Monday'
+    } if(num === 3 ){
+      return 'Tuesday'
+    }if(num === 4 ){
+      return "Wednesday"
+    } if(num === 5 ){
+      return "Thursday"
+    } if(num === 6 ){
+      return "Friday"
+    } if(num === 7 ){
+      return "Saturday"
+    } if (num < 1 || num > 7){
+      return 'Wrong, please enter a number between 1 and 7'
+    }
+  }
+
+  console.log(whatday(8))
