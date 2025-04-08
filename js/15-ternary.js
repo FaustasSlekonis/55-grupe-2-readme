a03bc5a8bf27768562dc618824aa44d564ac2189
Faustas <faustas.slekonis@gmail.com>
2025-04-08 10:56:15 +0300

    js scriptai

---
+++ js/15-ternary.js
@@ -0,0 +1,27 @@
+let zodis = ' ';
+
+if(4>2){
+    zodis = 'daugiau'
+}else{
+    zodis = 'maziau'
+}
+
+console.log(zodis)
+
+
+let skaicius = 0;
+
+if(4 <= 2){
+    skaicius = 7
+}else{
+    skaicius = 5
+}
+
+console.log(skaicius)
+
+
+const word = 4 > 2 ? 'daugiau' : 'maziau';
+console.log(word);
+
+const number = 7 <= 5 ? 7 : 5;
+console.log(number)
