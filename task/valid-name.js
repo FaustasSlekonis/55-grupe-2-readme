d252be6c3d2e4fd7661967b978c90a7d95c2b800
Faustas <faustas.slekonis@gmail.com>
2025-05-02 13:33:56 +0300

    string

--- task/valid-name.js
+++ task/valid-name.js
@@ -15,3 +15,52 @@ console.log(validName('jonas'), '-->', 'Jonas');
 console.log(validName('maRYTE'), '-->', 'Maryte');
 console.log(validName('PeTrAs'), '-->', 'Petras');
 console.log(validName('ONA'), '-->', 'Ona');
+
+
+
+
+let result = 10 / "2";
+console.log(result);
+
+
+console.log([1,2,3] + [4,5,6])
+
+
+
+console.log(typeof [])
+
+
+let arr = [1,2,3];
+console.log(arr.length);
+console.log("------------------------")
+
+let a = "5";
+let b = 2;
+console.log(a-b);
+
+
+let a1 = "HEllo"
+let b1 = a1;
+b1 = "Waorld";
+console.log(a1)
+
+
+let x = 10;
+if(x=5){
+    console.log("X yra 5")
+}
+
+
+
+let foo;
+console.log(foo);
+
+
+let numbers = [1,2,3];
+numbers[10] = 11;
+console.log(numbers.length);
+
+function add(a,b = 5){
+    return a+b;
+}
+console.log(add(10))
